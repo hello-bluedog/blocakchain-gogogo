44eb4d06fe9b7379aba35f2a8eb6eb15bcb2c9da
xiaohao111 <56581993+xiaohao111@users.noreply.github.com>
2023-03-31 02:23:36 +0800

    Fabric service 2.0
    增加了匿名性idemix

--- javacode/src/main/java/com/dataSharing/service/FabricService.java
+++ javacode/src/main/java/com/dataSharing/service/FabricService.java
@@ -1,6 +1,10 @@
 package com.dataSharing.service;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,6 +13,9 @@ import java.util.EnumSet;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeoutException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.hyperledger.fabric.gateway.Contract;
 import org.hyperledger.fabric.gateway.ContractException;
@@ -22,6 +29,7 @@ import org.hyperledger.fabric.gateway.X509Identity;
 import org.hyperledger.fabric.sdk.Enrollment;
 import org.hyperledger.fabric.sdk.Peer;
 import org.hyperledger.fabric.sdk.User;
+import org.hyperledger.fabric.sdk.identity.IdemixEnrollment;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
 import org.hyperledger.fabric.sdk.security.CryptoSuiteFactory;
 import org.hyperledger.fabric_ca.sdk.EnrollmentRequest;
@@ -29,6 +37,7 @@ import org.hyperledger.fabric_ca.sdk.HFCAClient;
 import org.hyperledger.fabric_ca.sdk.RegistrationRequest;
 import org.springframework.stereotype.Service;
 
+//import org.hyperledger.fabric.sdk.identity.IdemixEnrollmentSerialized;
 import com.dataSharing.entity.ledgerEntity.CoinNumAndCredit;
 
 @Service
@@ -43,8 +52,9 @@ public class FabricService {
 
 
 	public FabricService() throws Exception {
-		createAdmin();
-		login();
+		//createAdmin();
+	    createUser("ljh000");
+		//login();
 	}
 
 	public Network login() throws IOException {
@@ -57,11 +67,10 @@ public class FabricService {
 	    return network;
 	}
 
-	private HFCAClient getCaclient() throws Exception {
+	private static HFCAClient getCaclient() throws Exception {
 		// Create a CA client for interacting with the CA.
 	    Properties props = new Properties();
-	    props.put("pemFile",
-	                this.caPemFile);
+	    props.put("pemFile", "cert.pem");
 	    props.put("allowAllHostNames", "true");
 	    HFCAClient caClient = HFCAClient.createNewInstance("https://182.92.193.27:7054", props);
 	    CryptoSuite cryptoSuite = CryptoSuiteFactory.getDefault().getCryptoSuite();
@@ -94,9 +103,72 @@ public class FabricService {
 	    System.out.println("Successfully enrolled user \"admin\" and imported it into the wallet");
 	    return true;
 	}
+/* 
+	public class IdemixEnrollmentSerialized implements Enrollment {
 
+	    private static final String algo = "idemix";
 
-	public boolean createUser(String username) throws Exception {
+	    protected final String ipkSerializedString;
+	    protected final String revocationPkSerializedString;
+	    protected final String mspId;
+	    protected final String skSerializedString;
+	    protected final String credentialSerializedString;
+	    protected final String criSerializedString;
+	    protected final String ou;
+	    protected final String roleMask;
+
+	    public IdemixEnrollmentSerialized(String ipkSerializedString, String revocationPkSerializedString, String mspId, String skSerializedString, String credentialSerializedString, String criSerializedString, String ou, String roleMask) {
+	        this.ipkSerializedString = ipkSerializedString;
+	        this.revocationPkSerializedString = revocationPkSerializedString;
+	        this.mspId = mspId;
+	        this.skSerializedString = skSerializedString;
+	        this.credentialSerializedString = credentialSerializedString;
+	        this.criSerializedString = criSerializedString;
+	        this.ou = ou;
+	        this.roleMask = roleMask;
+	    }
+
+	    public PrivateKey getKey() {
+	        return null;
+	    }
+
+	    public String getCert() {
+	        return null;
+	    }
+
+	    public String getIpk() {
+	        return this.ipkSerializedString;
+	    }
+
+	    public String getRevocationPk() {
+	        return this.revocationPkSerializedString;
+	    }
+
+	    public String getMspId() {
+	        return this.mspId;
+	    }
+
+	    public String getSk() {
+	        return this.skSerializedString;
+	    }
+
+	    public String getCred() {
+	        return this.credentialSerializedString;
+	    }
+
+	    public String getCri() {
+	        return this.criSerializedString;
+	    }
+
+	    public String getOu() {
+	        return this.ou;
+	    }
+
+	    public String getRoleMask() {
+	        return this.roleMask;
+	    }
+	}*/
+	public static boolean createUser(String username) throws Exception {
 
 		    HFCAClient caClient = getCaclient();
 		    // 获取的钱包
@@ -156,11 +228,76 @@ public class FabricService {
 		    String enrollmentSecret = caClient.register(registrationRequest, admin);
 		    System.out.println(enrollmentSecret);
 		    Enrollment enrollment = caClient.enroll(username, enrollmentSecret);
-		    X509Identity user = Identities.newX509Identity("Org1MSP", enrollment);
-		    wallet.put(username, user);
-		    System.out.println("Successfully enrolled user \"appUser\" and imported it into the wallet");
+
+
+			IdemixEnrollment idemixEnrollment =  (IdemixEnrollment) caClient.idemixEnroll(enrollment, "Org1IdemixMSP");
+
+			//IdemixEnrollmentSerialized idemixEnrollment =  (IdemixEnrollmentSerialized) caClient.idemixEnroll(enrollment, "Org1IdemixMSP");
+			System.out.println("\nIdemix Enrollment IPK: " + idemixEnrollment.getIpk());
+			System.out.println("\nIdemix enrollment MSP: " + idemixEnrollment.getMspId());
+			Identity user = Identities.newX509Identity("Org1MSP", enrollment);
+			wallet.put(username, user);
+			Identity id =  Identities.newX509Identity("Org1IdemixMSP", idemixEnrollment);
+			wallet.put(username + "Idemix", id);
+			System.out.println("Successfully enrolled user " + username + " and imported it into the wallet");
+			
+/*	        X509Identity user = Identities.newX509Identity("Org1MSP", enrollment);
+		    IdemixEnrollment idemixEnrollment = (IdemixEnrollment) caClient.idemixEnroll(enrollment, "idemixMSPID1");
+		    Identity useridemix = Identities.newX509Identity("Org1IdemixMSP", idemixEnrollment);
+	        wallet.put(username, user);
+		    wallet.put(username + "idemix", useridemix);
+	        System.out.println("Successfully enrolled user \"appUser\" and imported it into the wallet");
+	        */
 		    return true;
 		}
+	 /**
+     * @Description:下载
+     * @Param downloadPath 文件路径
+     */
+    public static boolean downloadPathFile(String path, HttpServletRequest request, HttpServletResponse response) {
+        //设置文件路径
+        File file = new File(path);
+        //获取文件名称
+        String fileName = file.getName();
+        //判断文件是否存在
+        if (file.exists()) {
+            response.setContentType("application/force-download");// 设置强制下载不打开
+            response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+                file.delete();
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return false;
+    }
 	public static String AddNewUserItem(CoinNumAndCredit newUser) throws ContractException, TimeoutException, InterruptedException {
 		Contract contract = network.getContract(contractName);
 		byte[] result = contract.createTransaction("AddNewUserItem")
@@ -188,5 +325,19 @@ public class FabricService {
 		.submit(pkUser, "provider");
 		return new String(result, StandardCharsets.UTF_8);
 	}
-	
+	 public static String getProviderList(String roadName) throws ContractException {
+		 Contract contract = network.getContract(contractName);
+			byte[] result = contract.evaluateTransaction("GetMessageByRoadName", roadName);
+			return new String(result, StandardCharsets.UTF_8);
+	 }
+	 
+	 
+	public static String addNewMessage(String pkUser, String role) throws ContractException, TimeoutException, InterruptedException {
+			Contract contract = network.getContract(contractName);
+			byte[] result = contract.createTransaction("AddNewUserItem")
+			.setEndorsingPeers(network.getChannel().getPeers(EnumSet.of(Peer.PeerRole.ENDORSING_PEER)))
+			.submit(pkUser, "provider");
+			return new String(result, StandardCharsets.UTF_8);
+	}
+		
 }
